06fea59cf60647aceca7a0124bcfdc1ec0192a9a
Rose Ahmed <roseahmed2013@gmail.com>
2024-08-25 21:25:01 +0530

    github pages deployment setup

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export class Config {
-  x = "https://x.com/roseahmed54";
+  x = "https://www.x.com/roseahmed54";
   linkedin = "https://www.linkedin.com/in/rose-ahmed-5152b221a/";
   facebook = "https://www.facebook.com";
   instagram = "https://www.instagram.com";
